a8eda02371aa7d753611663c7d4c35e0788111bd
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2021-07-28 23:06:54 +0800

    Tried lemmatization

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from nltk.tokenize import sent_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import LancasterStemmer
 from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
 
 
 # tokenization
-
 nltk.download('punkt')
 
 sentence="This is the sentence. I am going to tokenize it"
@@ -33,3 +33,11 @@ print(Lancaster.stem('Funny'))
 
 Snow = SnowballStemmer('english')
 print(Snow.stem('Funny'))
+
+
+# Lemmatization
+nltk.download('wordnet')
+
+lem = WordNetLemmatizer()
+
+print(lem.lemmatize("kings"))
