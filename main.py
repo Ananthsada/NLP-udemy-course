64b6fd0a7004fb360c7099c78f4d686d9f14f345
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2021-07-30 23:54:06 +0800

    Added remove username and remove hyperlink function

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import re
 
-df = pd.loadcsv('dataset/Coachella-2015-2-DFE.csv')
-df.count()
+def removeUserName(text):
+    retText = re.sub('@[a-zA-z]+[a-zA-z0-9-_]', '', text)
+    return retText
+
+def removeHyperLink(text):
+    retText = re.sub(r'http\S+', '', text)
+    return retText
+
+df = pd.read_csv('dataset/Coachella-2015-2-DFE.csv', encoding='latin1')
+print(df['text'][1])
+sampleText = removeUserName(df['text'][1])
+print(sampleText)
+sampleText = removeHyperLink(sampleText)
+print(sampleText)
