3453f789c28ef4e8d0a35739aa9670c7ebaf6ab7
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2021-07-31 23:46:21 +0800

    Create new column hastag and new_tweet

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import re
+import nltk
+from nltk.corpus import stopwords
+#nltk.download('stopwords')
+
+def getHashtags(text):
+    retText = re.findall('#(\w+)', text)
+    return retText
 
 def removeUserName(text):
     retText = re.sub('@[a-zA-z]+[a-zA-z0-9-_]', '', text)
@@ -9,9 +16,29 @@ def removeHyperLink(text):
     retText = re.sub(r'http\S+', '', text)
     return retText
 
+def removeNonAscii(text):
+    retText = "".join(i for i in text if ord(i) < 128)
+    return retText
+
+def removeStopWords(text):
+    stops = set(stopwords.words('english'))
+    stops.update('mailto', 'this', 'it')
+    retText = ' '.join(i for i in text.split() if i not in stops)
+    return retText
+
+def convetLowerCase(text):
+    retText = text.lower()
+    return retText
+
+def removeEmailAddress(text):
+    retText = re.sub('[\w\.-]+@[\w]+.[\w]+', '', text)
+    return retText
+
 df = pd.read_csv('dataset/Coachella-2015-2-DFE.csv', encoding='latin1')
-print(df['text'][1])
-sampleText = removeUserName(df['text'][1])
-print(sampleText)
-sampleText = removeHyperLink(sampleText)
-print(sampleText)
+df['hastag'] = df.text.apply(func = getHashtags)
+df['new_tweet'] = df.text.apply(func = removeUserName)
+df['new_tweet'] = df.new_tweet.apply(func = removeHyperLink)
+df['new_tweet'] = df.new_tweet.apply(func = removeNonAscii)
+df['new_tweet'] = df.new_tweet.apply(func = convetLowerCase)
+df['new_tweet'] = df.new_tweet.apply(func = removeEmailAddress)
+print(df.head())
